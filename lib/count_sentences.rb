b18ce717d021d3c7578c91905ee54605f906224c
Dan Hintz <dhintz@live.com>
2018-09-02 17:16:15 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,11 +7,11 @@ class String
   end
 
   def question?
-    if self.end_with?("?")
+    self.end_with?("?")
   end
 
   def exclamation?
-    if self.end_with?("!")
+    self.end_with?("!")
   end
 
   def count_sentences
